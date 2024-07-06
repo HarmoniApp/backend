02955fc0bd6583524d03b8c24ecc290a1a22070a
Koter Michał (s24569) <91740818+Michal-Koter@users.noreply.github.com>
2024-07-06 12:09:33 +0200

    Initial setup (#2)
    * chore: add gitignore
    
    * chore: initia data module
    
    * chore: initia web api module
    
    * build: update data module application properties

---
+++ harmoni-data/src/test/java/org/harmoniapp/harmonidata/HarmoniDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harmoniapp.harmonidata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmoniDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/HarmoniWebApiApplication.java
@@ -0,0 +1,13 @@
+package org.harmoniapp.harmoniwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmoniWebApiApplication.class, args);
+    }
+
+}
