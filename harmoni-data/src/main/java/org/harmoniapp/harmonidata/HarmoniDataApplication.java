83c03a64720ff5ba95d31ea14efcb1b8c36a55ef
Koter Michal <s24569@pjwstk.edu.pl>
2024-07-06 11:35:39 +0200

    chore: initia data module

---
+++ harmoni-data/src/main/java/org/harmoniapp/harmonidata/HarmoniDataApplication.java
@@ -0,0 +1,13 @@
+package org.harmoniapp.harmonidata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarmoniDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarmoniDataApplication.class, args);
+    }
+
+}
