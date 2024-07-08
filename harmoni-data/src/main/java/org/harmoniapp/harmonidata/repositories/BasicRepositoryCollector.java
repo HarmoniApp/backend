fd0494b2b44777e3ffbdba34ae1311e58bf25377
Koter Michal <s24569@pjwstk.edu.pl>
2024-07-09 01:25:26 +0200

    feat: add repositories collector

---
+++ harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/BasicRepositoryCollector.java
@@ -0,0 +1,17 @@
+package org.harmoniapp.harmonidata.repositories;
+
+import lombok.Data;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Data
+public class BasicRepositoryCollector implements RepositoryCollector {
+    private final AbsenceRepository absences;
+    private final AddressRepository addresses;
+    private final AvailabilityRepository availabilities;
+    private final PredefineShiftRepository predefineShifts;
+    private final ShiftRepository shifts;
+    private final UserRepository users;
+    private final VacationRepository vacations;
+}
---
+++ harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/RepositoryCollector.java
@@ -0,0 +1,18 @@
+package org.harmoniapp.harmonidata.repositories;
+
+
+public interface RepositoryCollector {
+    AbsenceRepository getAbsences();
+
+    AddressRepository getAddresses();
+
+    AvailabilityRepository getAvailabilities();
+
+    PredefineShiftRepository getPredefineShifts();
+
+    ShiftRepository getShifts();
+
+    UserRepository getUsers();
+
+    VacationRepository getVacations();
+}
