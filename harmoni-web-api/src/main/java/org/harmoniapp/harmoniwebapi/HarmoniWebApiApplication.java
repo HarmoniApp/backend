a3a36d7599fde2b7b06ff050fc6009c4cf185566
Koter Michal <s24569@pjwstk.edu.pl>
2024-07-12 10:04:45 +0200

    fix: fix bug with bean initialization

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/HarmoniWebApiApplication.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/HarmoniWebApiApplication.java
@@ -2,8 +2,13 @@ package org.harmoniapp.harmoniwebapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication()
+//@ComponentScan(basePackages = "org.harmoniapp.harmonidata")
+//@EnableJpaRepositories(basePackages = "org.harmoniapp.harmonidata.repositories")
+
 public class HarmoniWebApiApplication {
 
     public static void main(String[] args) {
