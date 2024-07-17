6bada35a923a617de1f40de3ac87a07c98e6d8ed
Koter Michal <s24569@pjwstk.edu.pl>
2024-07-17 12:05:35 +0200

    refactor: use mapper to map entity to dto

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/services/UserService.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/services/UserService.java
@@ -1,25 +1,23 @@
 package org.harmoniapp.harmoniwebapi.services;
 
 import com.google.common.collect.Lists;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.harmoniapp.harmonidata.entities.Address;
 import org.harmoniapp.harmonidata.entities.User;
-import org.harmoniapp.harmonidata.entities.UserLanguage;
-import org.harmoniapp.harmonidata.entities.UserRole;
 import org.harmoniapp.harmonidata.repositories.RepositoryCollector;
-import org.harmoniapp.harmoniwebapi.contracts.AddressDto;
 import org.harmoniapp.harmoniwebapi.contracts.UserDto;
+import org.harmoniapp.harmoniwebapi.mappers.MapEntityDto;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"org.harmoniapp.harmonidata"})
 public class UserService {
     private final RepositoryCollector repositoryCollector;
+    private final MapEntityDto<User, UserDto> mapper;
     private final int page_size = 20;
 
     public List<UserDto> getUsers(int page) {
@@ -27,41 +25,19 @@ public class UserService {
         List<List<User>> pagedUsers = Lists.partition(users, page_size);
 
         return pagedUsers.get(page).stream()
-                .map(p -> new UserDto(
-                        p.getId(),
-                        p.getFirstname(),
-                        p.getSurname(),
-                        p.getEmail(),
-                        p.getContractType(),
-                        p.getContractSignature(),
-                        p.getContractExpiration(),
-                        mapAddress(p.getResidence()),
-                        mapAddress(p.getWorkAddress()),
-                        getSupervisor(p),
-                        p.getPhoneNumber(),
-                        p.getEmployeeId(),
-                        p.getRoles().stream().map(UserRole::getRole).collect(Collectors.toSet()),
-                        p.getLanguages().stream().map(UserLanguage::getLanguage).collect(Collectors.toSet())
-                        )
-                ).toList();
+                .map(mapper::toDto)
+                .toList();
     }
 
-    private AddressDto mapAddress(Address address) {
-        return new AddressDto(
-                address.getId(),
-                address.getZipCode(),
-                address.getCity(),
-                address.getStreet(),
-                address.getBuildingNumber(),
-                address.getApartment()
-        );
-    }
+    public UserDto getUser(long id) {
+        var userOptional = repositoryCollector.getUsers().findById(id);
 
-    private long getSupervisor(User user) {
-        long response = -1;
-        if (user.getSupervisor() != null) {
-            response = user.getSupervisor().getId();
+        if (userOptional.isEmpty()) {
+            throw new EntityNotFoundException();
         }
-        return response;
+
+        var user = userOptional.get();
+
+        return mapper.toDto(user);
     }
 }
