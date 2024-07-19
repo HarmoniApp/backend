0113c5f45168c48debee8b2384a9a37136638af9
Koter Michal <s24569@pjwstk.edu.pl>
2024-07-20 01:19:16 +0200

    feat: add user add, update, delete endpoints

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/controllers/UserController.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/controllers/UserController.java
@@ -3,8 +3,11 @@ package org.harmoniapp.harmoniwebapi.controllers;
 import lombok.RequiredArgsConstructor;
 import org.harmoniapp.harmoniwebapi.contracts.UserDto;
 import org.harmoniapp.harmoniwebapi.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,6 +24,24 @@ public class UserController {
     @GetMapping("/{id}")
     public UserDto getUser(@PathVariable long id) {
         return service.getUser(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addUser(@RequestBody UserDto userDto) {
+        service.add(userDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateUser(@PathVariable long id, @RequestBody UserDto userDto) {
+        long updatedId = service.update(id, userDto);
+        return ResponseEntity.created(URI.create("user/"+updatedId)).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 
