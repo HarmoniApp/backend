1715818a7ea510daf24438e75b00707b32b2e104
Koter Michal <s24569@pjwstk.edu.pl>
2024-08-05 18:20:21 +0200

    refactor: merge filtering to findUsers

--- harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/UserRepository.java
+++ harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/UserRepository.java
@@ -14,12 +14,12 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
 
     @Query("""
             select u from User u left join u.roles roles left join u.languages languages
-            where (u.contractType.id in ?1 or u.contractType.name in ?2 or (?1 is null and ?2 is null)) and
-                  (roles.id in ?3 or roles.name in ?4 or (?3 is null and ?4 is null)) and
-                  (languages.id in ?5 or languages.name in ?6 or (?5 is null and ?6 is null))""")
-    List<User> findAllByContractAndRoleAndLanguage(@Nullable Collection<Long> contractIds, @Nullable Collection<String> contracts,
-                                                   @Nullable Collection<Long> roleIds, @Nullable Collection<String> roles,
-                                                   @Nullable Collection<Long> languageIds, @Nullable Collection<String> languages,
+            where (u.contractType.id in ?1 or ?1 is null) and
+                  (roles.id in ?2 or ?2 is null) and
+                  (languages.id in ?3 or ?3 is null)""")
+    List<User> findAllByContractAndRoleAndLanguage(@Nullable Collection<Long> contract,
+                                                   @Nullable Collection<Long> role,
+                                                   @Nullable Collection<Long> language,
                                                    Sort sort);
 
     @Query("""
