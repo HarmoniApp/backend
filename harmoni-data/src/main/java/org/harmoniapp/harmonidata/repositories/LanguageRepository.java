833dd348641269f64dd6dad46da50f0121b248c8
Koter Michal <s24569@pjwstk.edu.pl>
2024-08-08 11:57:50 +0200

    feat: add language CRUD endpoints

--- harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/LanguageRepository.java
+++ harmoni-data/src/main/java/org/harmoniapp/harmonidata/repositories/LanguageRepository.java
@@ -3,5 +3,9 @@ package org.harmoniapp.harmonidata.repositories;
 import org.harmoniapp.harmonidata.entities.Language;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+    Optional<Language> findByNameIgnoreCase(String name);
 }
