60c7d48a3c70a3476f08d66422333742a72f20af
Koter Michal <s24569@pjwstk.edu.pl>
2024-08-19 17:29:25 +0200

    docs: add javadoc to classes associated with the supervisor

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/contracts/SupervisorDto.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/contracts/SupervisorDto.java
@@ -4,18 +4,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.harmoniapp.harmonidata.entities.Role;
 import org.harmoniapp.harmonidata.entities.User;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public record SupervisorDto(long id, String firstname, String surname, List<RoleDto> role, @JsonProperty("employee-id") String employeeId) {
+/**
+ * Data Transfer Object (DTO) representing a supervisor.
+ * This record encapsulates essential information about a supervisor.
+ *
+ * @param id         The unique identifier of the supervisor.
+ * @param firstname  The first name of the supervisor.
+ * @param surname    The surname of the supervisor.
+ * @param role       The list of roles associated with the supervisor.
+ * @param employeeId The employee ID of the supervisor.
+ */
+public record SupervisorDto(long id,
+                            String firstname,
+                            String surname,
+                            List<RoleDto> role,
+                            @JsonProperty("employee_id") String employeeId) {
 
+    /**
+     * Converts a {@link User} entity to a {@link SupervisorDto}.
+     * Filters roles based on the {@link Role#isSup()} method and maps them to {@link RoleDto}.
+     *
+     * @param user the {@link User} entity to convert.
+     * @return a {@link SupervisorDto} representing the user entity.
+     */
     public static SupervisorDto fromEntity(User user) {
-        List<RoleDto> roles = new ArrayList<>();
-        for (Role role : user.getRoles()) {
-            if (role.getId() <= 4) {
-                roles.add(RoleDto.fromEntity(role));
-            }
-        }
+        List<RoleDto> roles = user.getRoles().stream()
+                .filter(Role::isSup)
+                .map(RoleDto::fromEntity)
+                .toList();
 
         return new SupervisorDto(
                 user.getId(),
--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/services/SupervisorService.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/services/SupervisorService.java
@@ -5,17 +5,26 @@ import org.harmoniapp.harmonidata.entities.User;
 import org.harmoniapp.harmonidata.repositories.RepositoryCollector;
 import org.harmoniapp.harmoniwebapi.contracts.SupervisorDto;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Service class for managing supervisors.
+ * This service provides methods to retrieve supervisors from the repository.
+ */
 @Service
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"org.harmoniapp.harmonidata"})
 public class SupervisorService {
     private final RepositoryCollector repositories;
 
+    /**
+     * Retrieves all supervisors.
+     * It fetches supervisors from the repository and converts them to {@link SupervisorDto}.
+     *
+     * @return a list of {@link SupervisorDto} representing all supervisors.
+     */
     public List<SupervisorDto> getAllSupervisors(){
         List<User> users = repositories.getUsers().findSupervisors();
         return users.stream()
