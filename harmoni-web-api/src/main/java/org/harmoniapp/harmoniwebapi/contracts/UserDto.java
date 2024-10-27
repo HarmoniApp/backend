3181dc6e98fdf703377ec2957774805f1b88670b
MarMarcz <s24566@pjwstk.edu.pl>
2024-10-27 14:22:25 +0100

    fix: add unusedAbsenceDays field to userDto

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/contracts/UserDto.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/contracts/UserDto.java
@@ -85,6 +85,7 @@ public record UserDto(
         @JsonProperty("is_active") boolean isActive,
 
         @JsonProperty("available_absence_days") int availableAbsenceDays,
+        @JsonProperty("unused_absence_days") int unusedAbsenceDays,
 
         @NotEmpty(message = "Roles cannot be null or empty")
         List<Role> roles,
@@ -116,6 +117,7 @@ public record UserDto(
                 user.getPhoto(),
                 user.isActive(),
                 user.getAvailableAbsenceDays(),
+                user.getUnusedAbsenceDays(),
                 user.getRoles().stream().toList(),
                 user.getLanguages().stream().map(p -> new LanguageDto(p.getId(), p.getName(), p.getCode())).toList()
         );
