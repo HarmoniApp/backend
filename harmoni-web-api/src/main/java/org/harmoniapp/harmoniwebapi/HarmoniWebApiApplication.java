557bf1a779bc31bf6e670c85bba0a61cd0b9c260
MarMarcz <s24566@pjwstk.edu.pl>
2024-11-01 15:13:14 +0100

    feat: add DB variables to .env

--- harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/HarmoniWebApiApplication.java
+++ harmoni-web-api/src/main/java/org/harmoniapp/harmoniwebapi/HarmoniWebApiApplication.java
@@ -1,5 +1,6 @@
 package org.harmoniapp.harmoniwebapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HarmoniWebApiApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().filename(".env").load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
         SpringApplication.run(HarmoniWebApiApplication.class, args);
     }
 
